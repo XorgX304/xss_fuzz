823b6d7106421a3c3e6e0042d4c797a88db85a0d
blue-bird1 <diqiuonline@qq.com>
2017-10-01 20:36:15 +0800

    add datatype

---
+++ data/action.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python
+# coding:utf-8
+# Author qingniao
+
+from pdb import set_trace
+from lib.base import Base
+from lib.data import Xssdata
+
+
+class Char(Xssdata, Base):
+    __author__ = 'qingniao'
+    __doc__ = 'return action attribute'
+    __version__ = '1.0'
+
+    def __init__(self):
+        self._debug = False
+        self._data = [
+            'onafterprint',
+            'onbeforeprint',
+            'onbeforeunload',
+            'onerror',
+            'onhaschange',
+            'onload',
+            'onmessage',
+            'onoffline',
+            'ononline',
+            'onpagehide',
+            'onpageshow',
+            'onpopstate',
+            'onredo',
+            'onresize',
+            'onstorage',
+            'onundo',
+            'onunload',
+            'onblur',
+            'onchange',
+            'oncontextmenu',
+            'onfocus',
+            'onformchange',
+            'onforminput',
+            'oninput',
+            'oninvalid',
+            'onreset',
+            'onselect',
+            'onsubmit',
+            'onkeydown',
+            'onkeypress',
+            'onkeyup',
+            'onclick',
+            'ondblclick',
+            'ondrag',
+            'ondragend',
+            'ondragenter',
+            'ondragleave',
+            'ondragover',
+            'ondragstart',
+            'ondrop',
+            'onmousedown',
+            'onmousemove',
+            'onmouseout',
+            'onmouseover',
+            'onmouseup',
+            'onmousewheel',
+            'onscroll',
+            'onabort',
+            'oncanplay',
+            'oncanplaythrough',
+            'ondurationchange',
+            'onemptied',
+            'onended',
+            'onerror',
+            'onloadeddata',
+            'onloadedmetadata',
+            'onloadstart',
+            'onpause',
+            'onplay',
+            'onplaying',
+            'onprogress',
+            'onratechange',
+            'onreadystatechange',
+            'onseeked',
+            'onseeking',
+            'onstalled',
+            'onsuspend',
+            'ontimeupdate',
+            'onvolumechange',
+            'onwaiting',
+        ]
+
+    def get(self):
+        if self._debug:
+            set_trace()
+        for char in self._data:
+            yield char
+
+    def debug(self, level):
+        if self._debug:
+            self._debug = False
+            return False
+        self._debug = True
+        return True
+
+
